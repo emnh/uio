2601049dd54f20dc282ba1f360af74d2c6dffa10
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2014-09-03 15:31:14 +0200

    fix gaussian1.py

--- inf1100/week1/gaussian1.py
+++ inf1100/week1/gaussian1.py
@@ -2,27 +2,37 @@
 from math import sqrt, pi, exp
 from json import dumps
 
-def f(x):
-  m = 0
-  s = 2
-  ret = 1 / (sqrt(2) * pi * s)
+def gauss(x, m, s):
+  x = float(x)
+  m = float(m)
+  s = float(s)
+  ret = 1 / (sqrt(2 * pi) * s)
   ret *= exp(-0.5*((x - m) / s)**2)
   return ret
 
-# from http://stackoverflow.com/questions/477486/python-decimal-range-step-value
-def drange(start, stop, step):
-  x = start
-  while x < stop:
-    yield x
-    x += step
+print gauss(x=1,m=0,s=2)
 
-ds = []
-for i in drange(-4,4,0.1):
-  d = {
-      'x': i,
-      'y': f(i),
-      }
-  ds.append(d)
-print dumps(ds)
+# Run example
+'''
+0.176032663382
+'''
+
+# some debug stuff below, just ignore
+#
+# from http://stackoverflow.com/questions/477486/python-decimal-range-step-value
+#def drange(start, stop, step):
+#  x = start
+#  while x < stop:
+#    yield x
+#    x += step
+
+#ds = []
+#for i in drange(-4,4,0.1):
+#  d = {
+#      'x': i,
+#      'y': f(i),
+#      }
+#  ds.append(d)
+#print dumps(ds)
 
 # JSON display at http://jsbin.com/fosapo/2/edit
