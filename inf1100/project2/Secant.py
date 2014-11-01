b2d9c4a8259b524ef459124c692e112929815105
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2014-11-01 17:45:03 +0100

    finish Secant.py and root_finder_examples.py

--- inf1100/project2/Secant.py
+++ inf1100/project2/Secant.py
@@ -19,7 +19,7 @@ def iterate(f, x1, x2):
     fx1 = f(x1)
     fx2 = f(x2)
     if fx1 - fx2 == 0.0:
-        raise RootNotFoundException("division by zero in secant method: %.2f %.2f %.2f" % (x, x1, x2))
+        raise RootNotFoundException("division by zero in secant method: x1=%.2f x2=%.2f" % (x1, x2))
     x = x1 - fx1*(x1 - x2) / (fx1 - fx2)
     return [x, x1]
 
@@ -34,7 +34,7 @@ def secant(f, x1, x2, dfdx=None, epsilon=1.0E-7, N=100):
         yield [status, [x, x1, n]]
         [x, x1] = iterate(f, x, x1)
         n += 1
-    status = ('found secant: ~[x: %s, x1: %s, n: %d]' %
+    status = ('secant: ~[x: %s, x1: %s, n: %d]' %
                 (formatFloat(x), formatFloat(x1), n))
     yield [status, [x, x1, n]]
 
@@ -79,10 +79,23 @@ if __name__ == '__main__':
 
 # Run example
 '''
-trying to find zero near 0.0
-~0.0000000 after 1 iterations
-trying to find zero near 1.0
-~0.9610369 after 5 iterations
-trying to find zero near -1.0
-~-0.9610369 after 5 iterations
+trying to find zero in [-0.10, 0.10]
+secant: ~[x: -0.1000000, x1: 0.1000000, n: 0]
+secant: ~[x: 0.0000000, x1: -0.1000000, n: 1]
+trying to find zero in [-0.90, 1.10]
+secant: ~[x: -0.9000000, x1: 1.1000000, n: 0]
+secant: ~[x: -1.6325716, x1: -0.9000000, n: 1]
+secant: ~[x: -0.9130898, x1: -1.6325716, n: 2]
+secant: ~[x: -0.9235715, x1: -0.9130898, n: 3]
+secant: ~[x: -0.9662235, x1: -0.9235715, n: 4]
+secant: ~[x: -0.9605237, x1: -0.9662235, n: 5]
+secant: ~[x: -0.9610303, x1: -0.9605237, n: 6]
+secant: ~[x: -0.9610370, x1: -0.9610303, n: 7]
+trying to find zero in [-0.90, -1.10]
+secant: ~[x: -0.9000000, x1: -1.1000000, n: 0]
+secant: ~[x: -0.9422823, x1: -0.9000000, n: 1]
+secant: ~[x: -0.9643069, x1: -0.9422823, n: 2]
+secant: ~[x: -0.9608791, x1: -0.9643069, n: 3]
+secant: ~[x: -0.9610356, x1: -0.9608791, n: 4]
+secant: ~[x: -0.9610369, x1: -0.9610356, n: 5]
 '''
